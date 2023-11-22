64891c0aff6fe364cb084b71ba813cba474e163b
MartynasVCS <martynasvcs@gmail.com>
2023-11-22 10:05:29 +0200

    Remove unused imports

--- Framework/Pages/Common.cs
+++ Framework/Pages/Common.cs
@@ -5,8 +5,6 @@ using SeleniumExtras.WaitHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Framework.Pages
 {
